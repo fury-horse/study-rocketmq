c4da60f89fefe7e782d7f381e12890a1b81360a6
liangjun <420333870@qq.com>
2020-04-23 10:38:18 +0800

    first commit

---
+++ src/main/java/com/liangjun/study/Application.java
@@ -0,0 +1,14 @@
+package com.liangjun.study;
+
+
+/**
+* @desc    应用启动类
+* @version 1.0
+* @author  Liang Jun
+* @date    2020年04月23日 10:35:15
+**/
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("application start");
+    }
+}
